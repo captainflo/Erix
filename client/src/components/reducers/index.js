61d8f8931511d8342fc5fe404ea276a990353503
Florian Lahitte <lahitte.florian@gmail.com>
2021-11-02 15:46:17 -0400

    first fix

--- client/src/components/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import strains from './strains';
-
-export default combineReducers({
-  flowers: strains,
-});
--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
-import reducers from './components/reducers';
 import './index.css';
-import App from './components/App';
-import 'materialize-css/dist/css/materialize.min.css';
-import 'materialize-css/dist/js/materialize.min.js';
-
-const store = createStore(reducers, applyMiddleware(reduxThunk));
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
